2bc11635b3f3ea164c217fc249400957375df57d
calbch <calbch@protonmail.com>
2024-07-21 12:38:55 +0200

    introduce zit object storing

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 const sha1 = @import("std").crypto.hash.Sha1;
+const zlib = @import("std").compress.zlib;
 
-const zit_path: []const u8 = ".zit";
-const object_path: []const u8 = zit_path + "/objects/";
+const zit_dir: []const u8 = ".zit";
+const object_dir: []const u8 = zit_dir ++ "/objects";
 
 const ZitError = error{ ObjectNotFound, InvalidObjectType, IoError };
 
@@ -12,69 +13,79 @@ const ObjectType = enum {
     commit,
 
     pub fn toString(self: ObjectType) []const u8 {
-        return switch (self) {
-            .object => "object",
-            .tree => "tree",
-            .commit => "commit",
-        };
+        return @tagName(self);
     }
 };
 
 pub fn store_blob(path: []const u8) !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
-
     const allocator = arena.allocator();
 
-    // Open the file
-    const content = std.fs.cwd().readFileAlloc(allocator, path, std.math.maxInt(usize)) catch {
-        return ZitError.IoError;
-    };
-    defer allocator.free(content);
+    // Open the file to store as .zit object.
+    const file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
 
-    // Create the header according to the git docs
-    const header = try std.fmt.allocPrint(allocator, "blob {d}\u{0000}", .{content.len});
+    const file_size = try get_file_size(file);
+    const header = try create_blob_header(allocator, file_size);
     defer allocator.free(header);
 
-    const store = try std.mem.concat(allocator, u8, &[_][]const u8{ header, content });
-    defer allocator.free(store);
+    const hash = try calculate_sha1_hex(file, header, file_size);
+    const object_path = try create_object_path(allocator, hash);
+    defer allocator.free(object_path);
 
-    const hash = try create_sha1_hash(allocator, store);
-    defer allocator.free(hash);
+    try write_compressed_blob(file, object_path);
 
-    const dir_name = hash[0..2];
-    const file_name = hash[2..hash.len];
-
-    std.debug.print("header: {d}\ncontent: {d}\nheader: {d}\n", .{ header.len, content.len, store.len });
-    std.debug.print("file name: {s}/{s}", .{ dir_name, file_name });
+    std.debug.print("Blob stored at: {s}\n", .{object_path});
 }
 
-fn create_sha1_hash(data: []const u8) [40]u8 {
-    var sha_1: [sha1.digest_length]u8 = undefined;
-    sha1.hash(data, &sha_1, .{});
+fn get_file_size(file: std.fs.File) !u64 {
+    const file_stats = try file.stat();
+    return file_stats.size;
+}
 
+fn create_blob_header(allocator: std.mem.Allocator, file_size: u64) ![]u8 {
+    return try std.fmt.allocPrint(allocator, "blob {d}\u{0000}", .{file_size});
+}
+
+fn calculate_sha1_hex(file: std.fs.File, header: []const u8, file_size: u64) ![40]u8 {
+    var hasher = sha1.init(.{});
+    hasher.update(header);
+    var buffer: [4096]u8 = undefined;
+    var bytes_read: usize = 0;
+    while (bytes_read < file_size) {
+        const read_size = try file.read(buffer[0..]);
+        if (read_size == 0) break;
+        hasher.update(buffer[0..read_size]);
+        bytes_read += read_size;
+    }
+    const sha_1 = hasher.finalResult();
     return std.fmt.bytesToHex(sha_1, .lower);
 }
 
-fn create_object_path(hash: [40]u8) [41]u8 {
+fn create_object_path(allocator: std.mem.Allocator, hash_hex: [40]u8) ![]u8 {
     var path: [41]u8 = undefined;
     path[2] = '/';
-    std.mem.copyForwards(u8, path[0..2], hash[0..2]);
-    std.mem.copyForwards(u8, path[3..], hash[2..]);
-    return path;
+    std.mem.copyForwards(u8, path[0..2], hash_hex[0..2]);
+    std.mem.copyForwards(u8, path[3..], hash_hex[2..]);
+
+    return try std.fs.path.join(allocator, &[_][]const u8{ object_dir, &path });
 }
 
-test "object path test" {
-    const test_string = "foobarbaz";
-    const expected_path = "5f/5513f8822fdbe5145af33b64d8d970dcf95c6e";
+fn write_compressed_blob(file: std.fs.File, full_path: []const u8) !void {
+    const dirname = std.fs.path.dirname(full_path) orelse return error.IoError;
+    try std.fs.cwd().makePath(dirname);
 
-    const hash = create_sha1_hash(test_string);
-    const path = create_object_path(hash);
+    // Create out file for compressed blob.
+    const out_file = try std.fs.cwd().createFile(full_path, .{});
+    defer out_file.close();
 
-    try std.testing.expectEqualStrings(expected_path, &path);
+    // Reposition r/w offset
+    try file.seekTo(0);
+    var buffered_writer = std.io.bufferedWriter(out_file.writer());
+    try zlib.compress(file.reader(), buffered_writer.writer(), .{});
+    try buffered_writer.flush();
 }
-
-pub fn create_blob_header() []u8 {}
 
 pub fn main() !void {
     try store_blob("build.zig");
